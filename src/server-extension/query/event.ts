31a29c929672473b56716b0099793db203d90e8b
Preschian Febryantara <preschian27@gmail.com>
2022-10-28 22:09:03 +0700

    fix: name not empty string

--- src/server-extension/query/event.ts
+++ src/server-extension/query/event.ts
@@ -25,6 +25,8 @@ FROM event e
 WHERE
     e.interaction = $1
     AND ne.burned = false
+    AND ne.name <> ''
+    AND ne.name IS NOT NULL
     AND ce.name IS NOT NULL
     AND me.image IS NOT NULL
 GROUP BY ne.id, me.id, e.current_owner, me.image, ce.name
