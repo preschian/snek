9e47db2f4b48b8d79c9471b478d4f1418b7d9a59
Preschian Febryantara <preschian27@gmail.com>
2022-10-29 10:27:14 +0700

    chore: test wich cdn

--- src/mappings/utils/metadata.ts
+++ src/mappings/utils/metadata.ts
@@ -7,20 +7,7 @@ export const fetchMetadata = async <T>(metadata: string): Promise<T> => {
     if (!metadata) {
       return ensure<T>({});
     }
-    return await $obtain<T>(metadata, [
-      'aragon',
-      'cf',
-      'cloudflare',
-      'dweb',
-      'fleek',
-      'gateway',
-      'infura',
-      'ipfs',
-      'kodadot',
-      'nftstorage',
-      'pinata',
-      'rmrk',
-    ]);
+    return await $obtain<T>(metadata, ['kodadot']);
   } catch (e) {
     logger.warn('IPFS Err', e);
   }
